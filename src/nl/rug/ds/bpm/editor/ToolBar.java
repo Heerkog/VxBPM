cc86df867b8ba301376cfd09db9351b0190762e4
Heerkog <heerkogroefsema@gmail.com>
2017-01-24 11:33:50 +0100

    Converted toolbar to menubar

--- src/nl/rug/ds/bpm/editor/ToolBar.java
+++ src/nl/rug/ds/bpm/editor/ToolBar.java
@@ -30,11 +30,11 @@ public class ToolBar extends JMenuBar {
 
         JMenu fileMenu = new JMenu("File");
         JMenu editMenu = new JMenu("Edit");
-        JMenu optionsMenu = new JMenu("Options");
+        JMenu verificationMenu = new JMenu("Verification");
 
         this.add(fileMenu);
         this.add(editMenu);
-        this.add(optionsMenu);
+        this.add(verificationMenu);
 
         addMenuItem(fileMenu, "New", null).addActionListener(e -> {
             AppCore.app.clear();
@@ -52,11 +52,26 @@ public class ToolBar extends JMenuBar {
         addMenuItem(editMenu,"Undo", null).addActionListener(new BPMNEditorActions.HistoryAction(true));
         addMenuItem(editMenu,"Redo", null).addActionListener(new BPMNEditorActions.HistoryAction(false));
 
+
+        JMenuItem checkModel  = new JMenuItem("Verify");
+        checkModel.setBorderPainted(false);
+        checkModel.setEnabled(false);
+        verificationMenu.add(checkModel);
+        checkModel.addActionListener(e -> {
+            checkModel.setEnabled(false);
+            EventSource.fireEvent(EventType.CHECKMODEL_BUTTON_CLICK, null);
+        });
+        EventSource.addListener(EventType.CHECKMODEL_BUTTON_ENABLED, e -> {
+            checkModel.setEnabled((boolean) e);
+        });
+
+        verificationMenu.addSeparator();
+
         checkerGroup = new ButtonGroup();
         AppCore.app.config.getModelCheckers().forEach((item) -> {
             JRadioButtonMenuItem checker = new JRadioButtonMenuItem(item.getName(), true);
             checker.setActionCommand(item.getId());
-            optionsMenu.add(checker);
+            verificationMenu.add(checker);
             checkerGroup.add(checker);
             CellPropertyPanel.addChangeListener(checker, checkerGroup, e -> {
                 EventSource.fireEvent(EventType.MODEL_CHECKER_CHANGE, AppCore.app.config.getModelCheckers().stream().filter(
@@ -65,30 +80,11 @@ public class ToolBar extends JMenuBar {
             });
         });
 
+        verificationMenu.addSeparator();
 
-/*
-        comboBox = new JComboBox<String>();
-
-        AppCore.app.config.getModelCheckers().forEach((item) -> {
-            comboBox.addItem(item.getName());
-        });
-        comboBox.setMaximumSize(comboBox.getPreferredSize());
-
-        CellPropertyPanel.addChangeListener(comboBox, e -> {
-            int index = comboBox.getSelectedIndex();
-            EventSource.fireEvent(EventType.MODEL_CHECKER_CHANGE, AppCore.app.config.getModelCheckers().get(index));
-        });
-        comboBox.setSelectedIndex(0);
-        EventSource.fireEvent(EventType.MODEL_CHECKER_CHANGE, AppCore.app.config.getModelCheckers().get(0));
-        add(comboBox);
-*/
-
-        //add(Box.createHorizontalGlue());
-        optionsMenu.addSeparator();
-
-        autocheck = new JCheckBoxMenuItem("Auto-check ");
+        autocheck = new JCheckBoxMenuItem("Automatic Verification");
         autocheck.setSelected(true);
-        optionsMenu.add(autocheck);
+        verificationMenu.add(autocheck);
 
         fullAutoput = new JCheckBoxMenuItem("Full output");
         fullAutoput.setSelected(true);
@@ -98,48 +94,7 @@ public class ToolBar extends JMenuBar {
                 EventSource.fireEvent(EventType.CONSOLE_FULLOUTPUT_CHANGED, 0);
             }
         });
-        optionsMenu.add(fullAutoput);
-
-        JButton checkModel  = new JButton("CheckModel");
-        checkModel.setBorderPainted(false);
-        checkModel.setContentAreaFilled(false);
-        checkModel.setFocusable(false);
-        checkModel.setRolloverEnabled(true);
-        checkModel.getModel().addChangeListener(new ChangeListener()
-        {
-            @Override
-            public void stateChanged(ChangeEvent e)
-            {
-                ButtonModel model = (ButtonModel) e.getSource();
-
-                if(model.isRollover())
-                {
-                    checkModel.setBackground(new Color(145,201,247,128)); //Changes the colour of the button
-                    checkModel.setOpaque(true);
-                }
-
-                else
-                {
-                    checkModel.setBackground(null);
-                    checkModel.setOpaque(false);
-                }
-            }
-        });
-        JMenu sep = new JMenu("|");
-        sep.setEnabled(false);
-        this.add(sep);
-
-        this.add(checkModel);
-
-
-        checkModel.addActionListener(e -> {
-            checkModel.setEnabled(false);
-            EventSource.fireEvent(EventType.CHECKMODEL_BUTTON_CLICK, null);
-        });
-
-        EventSource.addListener(EventType.CHECKMODEL_BUTTON_ENABLED, e -> {
-            checkModel.setEnabled((boolean) e);
-        });
+        verificationMenu.add(fullAutoput);
     }
 
     public boolean isFullOutput() {
